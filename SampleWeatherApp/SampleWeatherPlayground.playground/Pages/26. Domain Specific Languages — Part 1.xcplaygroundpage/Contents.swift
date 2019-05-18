e524007df4932542a17183c733c2d90458bf76c0
Krzysztof Siejkowski <siejkowski@users.noreply.github.com>
2019-05-18 18:07:03 +0200

    Finished exercises for #26 (although the last one I cannot really understand)

--- SampleWeatherApp/SampleWeatherPlayground.playground/Pages/26. Domain Specific Languages — Part 1.xcplaygroundpage/Contents.swift
+++ SampleWeatherApp/SampleWeatherPlayground.playground/Pages/26. Domain Specific Languages — Part 1.xcplaygroundpage/Contents.swift
@@ -96,7 +96,7 @@ eval(simplify(.add(.mul(.var, 3), .mul(.var, 4))), with: 2)
  
  */
 
-func simplify2(_ expr: Expr) -> Expr {
+func simplify11(_ expr: Expr) -> Expr {
     switch expr {
     case .int:
         return expr
@@ -108,8 +108,9 @@ func simplify2(_ expr: Expr) -> Expr {
     }
 }
 
-print(simplify2(.add(.mul(1, 2), .mul(3, 2))))
-print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
+print(.add(.mul(1, 2), .mul(3, 2)))
+print(simplify11(.add(.mul(1, 2), .mul(3, 2))))
+print(simplify11(.add(.mul(1, .var), .mul(3, .var))))
 
 /*
  
@@ -118,7 +119,23 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
 
 */
 
+func simplify12(_ expr: Expr) -> Expr {
+    switch expr {
+    case .int:
+        return expr
+    case let .add(.mul(a, b), .mul(c, d)) where a == c, let .add(.mul(b, a), .mul(d, c)) where a == c:
+        return .mul(a, .add(b, d))
+    case .mul(1, let e), .mul(let e, 1): return e
+    case .add: return expr
+    case .mul: return expr
+    case .var: return expr
+    }
+}
 
+print(.mul(1, 2))
+print(simplify12(.mul(1, 2)))
+print(.mul(2, 1))
+print(simplify12(.mul(2, 1)))
 
 /*
 
@@ -127,7 +144,24 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
 
  */
 
+func simplify13(_ expr: Expr) -> Expr {
+    switch expr {
+    case .int:
+        return expr
+    case let .add(.mul(a, b), .mul(c, d)) where a == c, let .add(.mul(b, a), .mul(d, c)) where a == c:
+        return .mul(a, .add(b, d))
+    case .mul(1, let e), .mul(let e, 1): return e
+    case .mul(0, _), .mul(_, 0): return 0
+    case .add: return expr
+    case .mul: return expr
+    case .var: return expr
+    }
+}
 
+print(.mul(0, 2))
+print(simplify13(.mul(0, 2)))
+print(.mul(2, 0))
+print(simplify13(.mul(2, 0)))
 
 /*
  
@@ -136,7 +170,26 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
  
  */
 
+func simplify14(_ expr: Expr) -> Expr {
+    switch expr {
+    case .int:
+        return expr
+    case let .add(.mul(a, b), .mul(c, d)) where a == c, let .add(.mul(b, a), .mul(d, c)) where a == c:
+        return .mul(a, .add(b, d))
+    case .add(0, let e), .add(let e, 0): return e
+    case .mul(1, let e), .mul(let e, 1): return e
+    case .mul(0, _), .mul(_, 0): return 0
+    
+    case .add: return expr
+    case .mul: return expr
+    case .var: return expr
+    }
+}
 
+print(.add(0, 2))
+print(simplify14(.add(0, 2)))
+print(.add(2, 0))
+print(simplify14(.add(2, 0)))
 
 /*
  
@@ -145,7 +198,25 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
  
  */
 
+func simplify15(_ expr: Expr) -> Expr {
+    switch expr {
+    case .int: return expr
+        
+    case let .add(.mul(a, b), .mul(c, d)) where a == c, let .add(.mul(b, a), .mul(d, c)) where a == c:
+        return .mul(a, .add(b, d))
+    case .add(0, let e), .add(let e, 0): return e
+        
+    case let .mul(.add(a, b), .add(c, d)): return .add(.add(.mul(a, c), .mul(a, d)), .add(.mul(b, c), .mul(b, d)))
+    case .mul(1, let e), .mul(let e, 1): return e
+    case .mul(0, _), .mul(_, 0): return 0
+        
+    case .add: return expr
+    case .mul: return expr
+    case .var: return expr
+    }
+}
 
+print(simplify15(.mul(.add(5, 2), .add(3, 4))))
 
 /*
  
@@ -154,7 +225,90 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
  
  */
 
+enum Expr2: Equatable {
+    case int(Int)
+    case `var`(String)
+    indirect case add(Expr2, Expr2)
+    indirect case mul(Expr2, Expr2)
+}
 
+extension Expr2: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: Int) {
+        self = .int(value)
+    }
+}
+
+extension Expr2: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        self = .var(value)
+    }
+}
+
+func eval2(_ expr: Expr2, with values: [String: Int]) -> Int? {
+    switch expr {
+    case let .int(value):
+        return value
+    case let .add(lhs, rhs):
+        guard let first = eval2(lhs, with: values), let second = eval2(rhs, with: values) else { return nil }
+        return first + second
+    case let .mul(lhs, rhs):
+        guard let first = eval2(lhs, with: values), let second = eval2(rhs, with: values) else { return nil }
+        return first * second
+    case .var(let name):
+        return values[name]
+    }
+}
+
+func print2(_ expr: Expr2) -> String {
+    
+    switch expr {
+    case let .int(value):
+        return "\(value)"
+    case let .add(lhs, rhs):
+        return "(\(print2(lhs)) + \(print2(rhs)))"
+    case let .mul(lhs, rhs):
+        return "(\(print2(lhs)) * \(print2(rhs)))"
+    case .var(let name):
+        return name
+    }
+    
+}
+
+func swap2(_ expr: Expr2) -> Expr2 {
+    switch expr {
+    case .int:
+        return expr
+    case let .add(lhs, rhs):
+        return .mul(swap2(lhs), swap2(rhs))
+    case let .mul(lhs, rhs):
+        return .add(swap2(lhs), swap2(rhs))
+    case .var:
+        return expr
+    }
+}
+
+func simplify2(_ expr: Expr2) -> Expr2 {
+    switch expr {
+    case .int: return expr
+        
+    case let .add(.mul(a, b), .mul(c, d)) where a == c, let .add(.mul(b, a), .mul(d, c)) where a == c:
+        return .mul(a, .add(b, d))
+    case .add(0, let e), .add(let e, 0): return e
+        
+    case let .mul(.add(a, b), .add(c, d)): return .add(.add(.mul(a, c), .mul(a, d)), .add(.mul(b, c), .mul(b, d)))
+    case .mul(1, let e), .mul(let e, 1): return e
+    case .mul(0, _), .mul(_, 0): return 0
+        
+    case .add: return expr
+    case .mul: return expr
+    case .var: return expr
+    }
+}
+
+print2(.mul("x", .add(2, "y")))
+eval2(.mul("x", .add(2, "y")), with: ["x": 2, "y": 2])
+print2(.mul("x", "y"))
+eval2(.mul("x", "y"), with: ["x": 9, "y": 8])
 
 /*
  
@@ -163,7 +317,18 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
  
  */
 
+extension Expr2 {
+    static func + (lhs: Expr2, rhs: Expr2) -> Expr2 {
+        return .add(lhs, rhs)
+    }
+    
+    static func * (lhs: Expr2, rhs: Expr2) -> Expr2 {
+        return .mul(lhs, rhs)
+    }
+}
 
+print2("a" * "b")
+print2(1 + "a" * "b")
 
 /*
  
@@ -172,14 +337,49 @@ print(simplify2(.add(.mul(1, .var), .mul(3, .var))))
  
  */
 
+func varCount(_ expr: Expr2) -> Int {
+    switch expr {
+    case .var: return 1
+    case .int: return 0
+    case let .add(lhs, rhs), let .mul(lhs, rhs): return varCount(lhs) + varCount(rhs)
+    }
+}
 
+print2(2 * 3 + 1 + "a" * "b" * 5)
+varCount(2 * 3 + 1 + "a" * "b" * 5)
 
 /*
  
  Exercise 5.
  Write a pretty printer for Expr that adds a new line and indentation when printing the sub-expressions inside .add and .mul
  
+ .add(1, .mul(2, 3))
+ 
+ (
+   1 +
+     2 * 3
  */
 
+func prettyPrint(_ expr: Expr2) -> String {
+    
+    func actualPrint(expr: Expr2, indentation: Int, line: Bool) -> String {
+        let indent = String(repeating: " -> ", count: indentation)
+        let newLine = line ? "\n" : ""
+        let start = "\(newLine)\(indent)"
+        switch expr {
+        case let .int(value):
+            return "\(indent)\(value)"
+        case let .add(lhs, rhs):
+            return "\(start)(\(actualPrint(expr: lhs, indentation: 0, line: true)) + \(actualPrint(expr: rhs, indentation: 0, line: true)))"
+        case let .mul(lhs, rhs):
+            return "\(start)(\(actualPrint(expr: lhs, indentation: 0, line: true)) * \(actualPrint(expr: rhs, indentation: 0, line: true)))"
+        case .var(let name):
+            return "\(indent)\(name)"
+        }
+    }
+    return actualPrint(expr: expr, indentation: 0, line: false)
+}
+
+print(prettyPrint(1 * "x" + "y" * 4 + 123))
 
 //: [Next](@next)
